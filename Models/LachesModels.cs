2a09cd0ab8025bbcf82a0048581919e21f7d01a0
SergioBerge <sergioeaberge@gmail.com>
2023-12-15 09:32:04 -0300

    algooo

---
+++ Models/LachesModels.cs
@@ -0,0 +1,14 @@
+﻿namespace Lanches.MVC.Core.Models
+{
+    public class LachesModels
+    {
+        public int ID { get; set; }
+
+        public string Cliente { get; set; }
+
+        public string Lanche { get; set; }
+
+        public DateTime DateLanche { get; set; } = DateTime.Now;
+
+    }
+}
