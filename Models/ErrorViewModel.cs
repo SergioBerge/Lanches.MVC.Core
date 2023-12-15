5aa70a3dac71765ab5b87573f5bdfd90242ab755
SergioBerge <sergioeaberge@gmail.com>
2023-12-15 09:04:38 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lanches.MVC.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
