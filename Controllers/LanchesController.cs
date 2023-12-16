97358debffc8f18b197e73bfd89793fd58f55acd
SergioBerge <sergioeaberge@gmail.com>
2023-12-16 10:50:01 -0300

    data

---
+++ Controllers/LanchesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.MVC.Core.Controllers
+{
+    public class LanchesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- obj/Debug/net7.0/Lanches.MVC.Core.AssemblyInfo.cs
+++ obj/Debug/net7.0/Lanches.MVC.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lanches.MVC.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfc44ac93c33ed30bbeddbfb0f246dec677a5e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lanches.MVC.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lanches.MVC.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
